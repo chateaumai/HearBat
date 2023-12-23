8b6d19e4e601acf4467114a7ce535df161139f47
Daniel Tran <danieltran219@gmail.com>
2023-12-22 19:08:44 -0800

    added google tts util

--- lib/pages/module_types/words/word_path.dart
+++ lib/pages/module_types/words/word_path.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:hearbat/utils/google_tts.dart';
 
 class WordPath extends StatelessWidget {
-  const WordPath({super.key});
+  // Instance of GoogleTTS
+  final GoogleTTS googleTTS = GoogleTTS();
+
+  Future<void> playVoice(String text, String voiceType) async {
+    try {
+      await googleTTS.speak(text, voiceType);
+      // Add logic or UI update to indicate success if necessary
+    } catch (e) {
+      print("Error in playVoice: $e");
+      // Add logic or UI update to indicate failure if necessary
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Word Path'),
+        title: Text('Word Modules'),
       ),
       body: Center(
-        child: Text('Word Path'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () =>
+                  playVoice("Hello World", "en-US-Standard-B"), // First button
+              child: Text('Play "Hello"'),
+            ),
+            SizedBox(height: 20), // Adds space between the buttons
+            ElevatedButton(
+              onPressed: () => playVoice(
+                  "Goodbye World", "en-US-Standard-B"), // Second button
+              child: Text('Play "Goodbye"'),
+            ),
+            // Add more buttons or functionality as needed
+          ],
+        ),
       ),
     );
   }
---
+++ lib/utils/google_tts.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:googleapis/texttospeech/v1.dart' as tts;
+
+class GoogleTTS {
+  final AudioPlayer audioPlayer = AudioPlayer();
+
+  Future<String> _loadCredentials() async {
+    return await rootBundle.loadString('hearbat-408909-40d76f6c489d.json');
+  }
+
+  Future<void> speak(String text, String voicetype) async {
+    http.Client client = http.Client();
+    try {
+      String jsonString = await _loadCredentials();
+      var jsonCredentials = jsonDecode(jsonString);
+
+      final accountCredentials =
+          ServiceAccountCredentials.fromJson(jsonCredentials);
+      AccessCredentials credentials =
+          await obtainAccessCredentialsViaServiceAccount(accountCredentials,
+              [tts.TexttospeechApi.cloudPlatformScope], client);
+
+      String url = "https://texttospeech.googleapis.com/v1/text:synthesize";
+      var body = json.encode({
+        "audioConfig": {
+          "audioEncoding": "LINEAR16",
+          "pitch": 0,
+          "speakingRate": 1
+        },
+        "input": {"text": text},
+        "voice": {"languageCode": "en-US", "name": voicetype}
+      });
+
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer ${credentials.accessToken.data}"
+        },
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(response.body);
+        String audioBase64 = jsonData['audioContent'];
+
+        Uint8List bytes = base64Decode(audioBase64);
+        String dir = (await getApplicationDocumentsDirectory()).path;
+        File file = File("$dir/${DateTime.now().millisecondsSinceEpoch}.mp3");
+        await file.writeAsBytes(bytes);
+
+        await audioPlayer.play(DeviceFileSource(file.path));
+      } else {
+        throw Exception(
+            "Failed to get a valid response from the API: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error in GoogleTTS.speak: $e");
+      throw Exception("Error occurred in GoogleTTS.speak: $e");
+    } finally {
+      client.close();
+    }
+  }
+}
