61d4d3dd78761e6604cf90466d51304aa79034f1
Daniel Tran <danieltran219@gmail.com>
2024-01-02 19:40:40 -0800

    adding cache system to google tts

--- lib/utils/google_tts_util.dart
+++ lib/utils/google_tts_util.dart
@@ -15,9 +15,22 @@ class GoogleTTSUtil {
     return await rootBundle.loadString('hearbat-408909-40d76f6c489d.json');
   }
 
+  Future<String> _getCacheFilePath(String text) async {
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    return "$dir/${text.hashCode}.mp3"; // Use text's hashcode as the file name
+  }
+
   Future<void> speak(String text, String voicetype) async {
     http.Client client = http.Client();
     try {
+      String cacheFilePath = await _getCacheFilePath(text);
+
+      // Check if the cached MP3 file exists
+      if (await File(cacheFilePath).exists()) {
+        await audioPlayer.play(DeviceFileSource(cacheFilePath));
+        return; // Return if the cached file is played
+      }
+
       String jsonString = await _loadCredentials();
       var jsonCredentials = jsonDecode(jsonString);
 
@@ -26,7 +39,6 @@ class GoogleTTSUtil {
       AccessCredentials credentials =
           await obtainAccessCredentialsViaServiceAccount(accountCredentials,
               [tts.TexttospeechApi.cloudPlatformScope], client);
-
       String url = "https://texttospeech.googleapis.com/v1/text:synthesize";
       var body = json.encode({
         "audioConfig": {
@@ -52,11 +64,12 @@ class GoogleTTSUtil {
         String audioBase64 = jsonData['audioContent'];
 
         Uint8List bytes = base64Decode(audioBase64);
-        String dir = (await getApplicationDocumentsDirectory()).path;
-        File file = File("$dir/${DateTime.now().millisecondsSinceEpoch}.mp3");
-        await file.writeAsBytes(bytes);
 
-        await audioPlayer.play(DeviceFileSource(file.path));
+        // Save the generated audio to the cache file
+        File cacheFile = File(cacheFilePath);
+        await cacheFile.writeAsBytes(bytes);
+
+        await audioPlayer.play(DeviceFileSource(cacheFilePath));
       } else {
         throw Exception(
             "Failed to get a valid response from the API: ${response.statusCode}");
