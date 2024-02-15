dbc17c5bd6eedeef022a1979d5572a67b6951afd
Daniel Tran <danieltran219@gmail.com>
2024-02-14 18:03:47 -0800

    adding jazz music to intro page

--- lib/pages/sound_adjustment_page.dart
+++ lib/pages/sound_adjustment_page.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'navigation_bar.dart';
 
-class SoundAdjustmentPage extends StatelessWidget {
+class SoundAdjustmentPage extends StatefulWidget {
+  @override
+  SoundAdjustmentPageState createState() => SoundAdjustmentPageState();
+}
+
+class SoundAdjustmentPageState extends State<SoundAdjustmentPage> {
+  final AudioPlayer audioPlayer = AudioPlayer();
+  final String audioPath = 'audio/background/jazz.mp3';
+
+  @override
+  void initState() {
+    super.initState();
+    playSound();
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.stop();
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void playSound() async {
+    await audioPlayer.play(AssetSource(audioPath));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
