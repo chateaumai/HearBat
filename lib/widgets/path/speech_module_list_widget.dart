1e7d5e000684f0bde981b10af37b6d74c90a851c
Daniel Tran <dhtran.hy@gmail.com>
2024-05-05 13:18:51 -0700

    correcting voice type for STT

--- lib/widgets/path/speech_module_list_widget.dart
+++ lib/widgets/path/speech_module_list_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../module/speech_module_widget.dart';
 import 'sound_trangular_path_layout_widget.dart';
 import 'animated_button_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SpeechModuleListWidget extends StatelessWidget {
   final Map<String, List<String>> modules;
@@ -11,6 +12,12 @@ class SpeechModuleListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var moduleList = modules.entries.toList();
+    String voiceType = '';
+
+    SharedPreferences.getInstance().then((SharedPreferences prefs) {
+      voiceType = prefs.getString('voiceType') ?? 'en-US-Wavenet-D';
+    });
+
     void navigate(String moduleName, List<String> sentences) {
       Navigator.push(
         context,
@@ -18,6 +25,7 @@ class SpeechModuleListWidget extends StatelessWidget {
           builder: (context) => SpeechModuleWidget(
             chapter: moduleName,
             sentences: sentences,
+            voiceType: voiceType,
           ),
         ),
       );
