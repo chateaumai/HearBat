a83066344b6644646a718e0fd66e28404084d373
Luke Walker <lukeryanwalker2000@gmail.com>
2025-02-18 19:30:04 -0800

    Fix custom module builder not using LLM
    Currently uses Gemini 2.0 Flash

--- lib/utils/gemini_util.dart
+++ lib/utils/gemini_util.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:firebase_vertexai/firebase_vertexai.dart';
 import 'package:http/http.dart' as http;
 import 'package:googleapis_auth/auth_io.dart';
+
 import '../utils/text_util.dart';
 import '../utils/config_util.dart';
 
@@ -16,89 +18,18 @@ class GeminiUtil {
   static Future<String> generateContent(List<String> wordsToBeCompared) async {
     http.Client client = http.Client();
     try {
-      String jsonString = await _loadCredentials();
-      var jsonCredentials = jsonDecode(jsonString);
-      String projectId = jsonCredentials['project_id'];
-      print(projectId);
-
-      // Authenticates using Google Service Account credentials.
-      final accountCredentials =
-          ServiceAccountCredentials.fromJson(jsonCredentials);
-      AccessCredentials credentials =
-          await obtainAccessCredentialsViaServiceAccount(accountCredentials,
-              ['https://www.googleapis.com/auth/cloud-platform'], client);
-
-      String url =
-          "https://us-west1-aiplatform.googleapis.com/v1/projects/$projectId/locations/us-west1/publishers/google/models/gemini-pro:streamGenerateContent";
-
       // Prepares the user input and determines the number of additional words needed.
       String wordInput = wordsToBeCompared.join(' ');
       int wordsNeeded = 4 - wordsToBeCompared.length;
       String prompt = getPrompt(wordsNeeded, wordInput);
-      print(prompt);
+      print(prompt); // for debug
 
-      // Creates the request payload with AI model parameters.
-      var body = json.encode({
-        "contents": [
-          {
-            "role": "USER",
-            "parts": [
-              {"text": prompt}
-            ]
-          }
-        ],
-        "safetySettings": [
-          {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_NONE"
-          },
-          {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
-          {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
-          {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_NONE"
-          },
-        ],
-        "generationConfig": {
-          "temperature": 0.9,
-          "candidateCount": 1,
-          "maxOutputTokens": 50,
-          "topP": 0.0,
-        }
-      });
-      // print(prompt);
-      final response = await http.post(
-        Uri.parse(url),
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${credentials.accessToken.data}"
-        },
-        body: body,
-      );
+      // Prompt the LLM
+      final model = FirebaseVertexAI.instance.generativeModel(model: 'gemini-2.0-flash');
+      final response = await model.generateContent([Content.text(prompt)]);
+      print(response.text);
 
-      if (response.statusCode == 200) {
-        var jsonData = jsonDecode(response.body);
-
-        List<String> allTexts = [];
-
-        // Extracts text content from the API response.
-        for (var candidateData in jsonData) {
-          for (var candidate in candidateData['candidates']) {
-            for (var part in candidate['content']['parts']) {
-              if (part.containsKey('text')) {
-                allTexts.add(part['text']);
-              }
-            }
-          }
-        }
-
-        // Combines all generated texts into a single string.
-        String combinedTexts = allTexts.join(' ');
-        return combinedTexts;
-      } else {
-        throw Exception(
-            "Failed to get a valid response from the API: ${response.statusCode}");
-      }
+      return response?.text ?? "";
     } catch (e) {
       print("Error in GeminiUtil.generateContent: $e");
       throw Exception("Error occurred in GeminiUtil.generateContent: $e");
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import audioplayers_darwin
 import firebase_analytics
+import firebase_app_check
+import firebase_auth
 import firebase_core
 import firebase_remote_config
 import path_provider_foundation
@@ -15,6 +17,8 @@ import shared_preferences_foundation
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
