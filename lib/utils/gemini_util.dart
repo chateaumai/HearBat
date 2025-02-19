a357910de499707a0b33e2a8d89400c753b81c13
Luke Walker <lukeryanwalker2000@gmail.com>
2025-02-18 22:26:51 -0800

    Remove unused code and debug statements

--- lib/utils/gemini_util.dart
+++ lib/utils/gemini_util.dart
@@ -7,27 +7,19 @@ import '../utils/text_util.dart';
 import '../utils/config_util.dart';
 
 class GeminiUtil {
-  // Loads the Google Cloud API key from the configuration manager.
-  static Future<String> _loadCredentials() async {
-    String apiKey = ConfigurationManager().googleCloudAPIKey;
-    return apiKey;
-  }
-
   // Generates AI-generated content based on a list of words.
   // It connects to the Google Gemini AI model to generate additional words.
   static Future<String> generateContent(List<String> wordsToBeCompared) async {
     http.Client client = http.Client();
     try {
       // Prepares the user input and determines the number of additional words needed.
-      String wordInput = wordsToBeCompared.join(' ');
-      int wordsNeeded = 4 - wordsToBeCompared.length;
-      String prompt = getPrompt(wordsNeeded, wordInput);
-      print(prompt); // for debug
+      final wordInput = wordsToBeCompared.join(' ');
+      final wordsNeeded = 4 - wordsToBeCompared.length;
+      final prompt = getPrompt(wordsNeeded, wordInput);
 
       // Prompt the LLM
       final model = FirebaseVertexAI.instance.generativeModel(model: 'gemini-2.0-flash');
       final response = await model.generateContent([Content.text(prompt)]);
-      print(response.text);
 
       return response?.text ?? "";
     } catch (e) {
