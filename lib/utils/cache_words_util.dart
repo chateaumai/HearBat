08d3de59275ba342a06fc9bf8a8548ff1ab87c54
Daniel Tran <dhtran.hy@gmail.com>
2025-02-19 23:39:03 -0800

    Fixing caching logic for normal and hard mode for word modules

--- lib/utils/cache_words_util.dart
+++ lib/utils/cache_words_util.dart
@@ -1,13 +1,28 @@
 import 'package:hearbat/utils/google_tts_util.dart';
 import 'package:hearbat/data/answer_pair.dart';
 import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
 
+// Utility for caching words and sentences for a module by downloading their audio using Google TTS
 class CacheWordsUtil {
   final GoogleTTSUtil googleTTSUtil = GoogleTTSUtil();
+  bool _isHardMode = false;
+
+  CacheWordsUtil() {
+    _loadDifficultyPreference();
+  }
+
+  // Loads the difficulty preference to determine whether we are in Hard Mode.
+  Future<void> _loadDifficultyPreference() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _isHardMode = prefs.getString('difficultyPreference') == 'Hard';
+  }
 
   // Caches words from a module by downloading their audio using Google TTS.
   Future<void> cacheModuleWords(
       List<AnswerGroup> module, String voiceType) async {
+    await _loadDifficultyPreference(); // Ensure we load the latest mode
+
     // Create a list to hold all the futures for the concurrent downloads
     List<Future> downloadFutures = [];
 
@@ -19,10 +34,17 @@ class CacheWordsUtil {
         group.answer4
       ];
       for (var answer in answers) {
+        String textToCache = answer.answer;
+
+        // If in Hard Mode, modify the text before caching
+        if (_isHardMode) {
+          textToCache = "Please select ${answer.answer} as the answer";
+        }
+
         // Instead of awaiting each download, add it to the list
         downloadFutures.add(
-            googleTTSUtil.downloadMP3(answer.answer, voiceType).catchError((e) {
-          print("Error downloading ${answer.answer}: $e");
+            googleTTSUtil.downloadMP3(textToCache, voiceType).catchError((e) {
+          print("Error downloading $textToCache: $e");
         }));
       }
     }
